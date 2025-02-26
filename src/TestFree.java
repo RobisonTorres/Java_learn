5cc7d4630558cbe672cf5906101debbd82558752
RobisonTorres <robison.torres21@gmail.com>
2025-02-26 10:45:21 -0300

    Test commit.

--- src/TestFree.java
+++ src/TestFree.java
@@ -2,7 +2,7 @@ public class TestFree {
 
     public static void main (String[] args){
 
-        String test = "This is a simple test";
+        String test = "This is a simple test...";
         System.out.println("This is a simple test...");
         System.out.println(test);
     }
